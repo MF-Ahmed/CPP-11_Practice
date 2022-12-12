3b58af09f669dcfeedae347279d2887044939d1f
MF-Ahmed <itsfarhan@outlook.com>
2022-12-12 15:18:52 +0100

    added CMake_config

---
+++ CMake_config_tutorial/our_project/build/our_project/tools/CMakeFiles/tools.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/data/git/CPP-11_Practice/CMake_config_tutorial/our_project/our_project/tools/tools.cpp" "/home/user/data/git/CPP-11_Practice/CMake_config_tutorial/our_project/build/our_project/tools/CMakeFiles/tools.dir/tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
